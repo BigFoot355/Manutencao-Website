151a4f76a777be9a4a0164f1e41ffceb4d12f4b4
AlexandreEscola <alcoliveira@es-loule.edu.pt>
2025-03-27 23:14:06 +0000

    postgresql

--- manutencao/settings.py
+++ manutencao/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+import dj_database_url  # Para facilitar a conexão com PostgreSQL
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -74,6 +76,10 @@ WSGI_APPLICATION = 'manutencao.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+DATABASES = {
+    'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
+}
+
 #DATABASES = {
 #    'default': {
 #        'ENGINE': 'django.db.backends.sqlite3',
@@ -81,16 +87,16 @@ WSGI_APPLICATION = 'manutencao.wsgi.application'
 #    }
 #}
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'manutencao_bd',
-        'USER': 'manutencao_user',
-        'PASSWORD': 'eKqM9FQFVZXlJBOJv0po7aM22jnEs5CY',
-        'HOST': 'localhost',  # ou o IP do servidor PostgreSQL
-        'PORT': '5432',
-    }
-}
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.postgresql',
+#        'NAME': 'manutencao_bd',
+#        'USER': 'manutencao_user',
+#        'PASSWORD': 'eKqM9FQFVZXlJBOJv0po7aM22jnEs5CY',
+#        'HOST': 'localhost',  # ou o IP do servidor PostgreSQL
+#        'PORT': '5432',
+#    }
+#}
 
 
 # Password validation

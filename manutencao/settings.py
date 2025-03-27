655c21d25029dce4d72a2ce3abd7e9113aa841a4
AlexandreEscola <alcoliveira@es-loule.edu.pt>
2025-03-27 21:21:39 +0000

    postgresql

--- manutencao/settings.py
+++ manutencao/settings.py
@@ -74,10 +74,21 @@ WSGI_APPLICATION = 'manutencao.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'manutencao_bd',
+        'USER': 'manutencao_user',
+        'PASSWORD': 'bd#123',
+        'HOST': 'localhost',  # ou o IP do servidor PostgreSQL
+        'PORT': '5432',
     }
 }
 

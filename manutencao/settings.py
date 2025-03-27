8956a1d06aa50236115e42e4e39c0028bfd4e5af
AlexandreEscola <alcoliveira@es-loule.edu.pt>
2025-03-27 23:30:05 +0000

    Specify PostgreSQL engine in database configuration

--- manutencao/settings.py
+++ manutencao/settings.py
@@ -77,7 +77,10 @@ WSGI_APPLICATION = 'manutencao.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
+    'default': dj_database_url.config(
+        default=os.getenv('DATABASE_URL'),
+        engine='django.db.backends.postgresql'  # Define explicitamente o engine
+    )
 }
 
 #DATABASES = {
